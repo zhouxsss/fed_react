a86d2bcab2636b74f900024c1623316266985ddc
wangshenjiao <wangsj@yunphant.com>
2019-11-27 17:18:28 +0800

    feat: add some code to test pre-commit

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { FormattedMessage } from 'i18n'
 
-const About = () => {
+import styles from './About.module.scss'
+
+const About = props => {
+  const addOne = () => {
+    props.dispatch({
+      type: 'count/increment'
+    })
+  }
+
+  const minusOne = () => {
+    props.dispatch({
+      type: 'count/decrement'
+    })
+  }
+
+  const asyncDecrementOne = () => {
+    props.dispatch({
+      type: 'count/asyncDecrement'
+    })
+  }
+
   return (
-    <div>
+    <div className={styles.about}>
       <h4>
         <FormattedMessage id="about_title" plain />
       </h4>
+      <p>{`count: ${props.count}`}</p>
+      <div>
+        <button onClick={addOne}>click +1</button>
+        <button onClick={minusOne}>click -1</button>
+        <button onClick={asyncDecrementOne}>click -1 after 2s</button>
+      </div>
     </div>
   )
 }
 
-export default About
+export default connect(({ count: { count } }) => ({ count }))(About)
